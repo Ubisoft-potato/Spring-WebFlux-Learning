dc94c5d1e6ce37a0ad5f6d92bb5b482d40bb46eb
cyka <1354250064mdzz@gmail.com>
2020-08-16 16:28:42 +0800

    add dependency

--- src/main/java/org/cyka/controller/HelloController.java
+++ src/main/java/org/cyka/controller/HelloController.java
@@ -3,13 +3,13 @@ package org.cyka.controller;
 import org.reactivestreams.Publisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class HelloController {
 
   @GetMapping("/hello")
   public Publisher<String> sayHello() {
-    return Mono.just("hello");
+    return Flux.<String>generate(sink -> sink.next("hello\n")).take(20);
   }
 }
