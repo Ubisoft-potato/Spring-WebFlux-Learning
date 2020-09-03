3523b71599663cc037fb4bd698e013828a8e3a0d
cyka <1354250064mdzz@gmail.com>
2020-09-03 22:43:57 +0800

    webflux cloud interaction

--- webFlux-provider/src/main/java/org/cyka/WebFluxProviderApplication.java
+++ webFlux-provider/src/main/java/org/cyka/WebFluxProviderApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebFluxApplication {
+public class WebFluxProviderApplication {
   public static void main(String[] args) {
-    SpringApplication.run(WebFluxApplication.class, args);
+    SpringApplication.run(WebFluxProviderApplication.class, args);
   }
 }
