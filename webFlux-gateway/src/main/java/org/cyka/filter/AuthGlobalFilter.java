cf59690188f29ab543a38b6df92d69d11bd695e2
cyka <1354250064mdzz@gmail.com>
2020-09-09 22:31:33 +0800

    add gateway filter

---
+++ webFlux-gateway/src/main/java/org/cyka/filter/AuthGlobalFilter.java
@@ -0,0 +1,15 @@
+package org.cyka.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthGlobalFilter implements GlobalFilter {
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    return null;
+  }
+}
