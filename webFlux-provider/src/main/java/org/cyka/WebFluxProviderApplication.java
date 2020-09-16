47b5d352353e08fdd45cceafbbd85b9ea390fac3
cyka <1354250064mdzz@gmail.com>
2020-09-16 21:31:56 +0800

    using fluent springApplication api

--- webFlux-provider/src/main/java/org/cyka/WebFluxProviderApplication.java
+++ webFlux-provider/src/main/java/org/cyka/WebFluxProviderApplication.java
@@ -1,11 +1,20 @@
 package org.cyka;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WebFluxProviderApplication {
   public static void main(String[] args) {
-    SpringApplication.run(WebFluxProviderApplication.class, args);
+    new SpringApplicationBuilder(WebFluxProviderApplication.class) // configuration class
+        .main(WebFluxProviderApplication.class) // just main class, not for config
+        .bannerMode(Banner.Mode.CONSOLE)
+        .web(WebApplicationType.REACTIVE)
+        .logStartupInfo(true)
+        .registerShutdownHook(true)
+        .build()
+        .run(args);
   }
 }
